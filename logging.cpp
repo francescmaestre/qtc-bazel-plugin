1bb9bb11f338cd3b9be7f2c73e7ea4e7c5252196
Max Breev <knuckster@gmail.com>
2022-12-21 01:22:50 +0100

    Add helper class `ScopedStopwatchLogger`
    This is intended for simple perf-measurement purposes.

--- logging.cpp
+++ logging.cpp
@@ -6,4 +6,18 @@ namespace BazelProjectManager::Internal
 
 Q_LOGGING_CATEGORY(BazelPluginLog, "qtc.bazel_plugin")
 
+ScopedStopwatchLogger::ScopedStopwatchLogger(std::string explanation)
+  : explanation_{std::move(explanation)},
+    startTime_{std::chrono::steady_clock::now()}
+{
+}
+
+ScopedStopwatchLogger::~ScopedStopwatchLogger() {
+  // TODO: Make resolution a class template parameter.
+  const auto elapsedMillis = std::chrono::duration_cast<std::chrono::milliseconds>(
+    std::chrono::steady_clock::now() - startTime_
+  );
+  qCInfo(BazelPluginLog) << explanation_.c_str() << ": " << elapsedMillis.count() << "ms";
+}
+
 }
