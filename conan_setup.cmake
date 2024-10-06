558cb4e6295eccb2e61f610042e05a5038e7d9ec
Max Breev <knuckster@gmail.com>
2024-10-06 22:50:33 +0200

    Fixup conanfile to also produce CMake Find modules
    Previously this was not the case and thus was not always sufficient to
    find depedencies.

--- conan_setup.cmake
+++ conan_setup.cmake
@@ -6,7 +6,7 @@ file(
 
 set(conan_dir "${CMAKE_BINARY_DIR}/conan")
 if (NOT EXISTS "${conan_paths_file}")
-  message(STATUS "Could not find conan_paths.cmake. Running it for you...")
+  message(STATUS "Could not find conan_paths.cmake. Running Conan for you...")
 
   file(MAKE_DIRECTORY "${conan_dir}")
 
@@ -18,7 +18,16 @@ if (NOT EXISTS "${conan_paths_file}")
     RESULT_VARIABLE conan_install_result
   )
 
+  if (NOT conan_install_result EQUAL 0)
+    message(FATAL_ERROR "Failed to install conan packages.")
+  endif()
+
   set(conan_paths_file "${conan_dir}/conan_paths.cmake")
+endif()
+
+if (NOT EXISTS "${conan_paths_file}")
+  message(FATAL_ERROR "File ${conan_paths_file} still does not exist, although it should have been \
+created by Conan.")
 endif()
 
 # Enable us to find libraries installed by conan.
